00321303584e679ba3d9f2212231736e784d4f48
Enrico Deleo <hello@enricodeleo.com>
2020-12-20 20:04:20 +0100

    feature: darkmode

---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    darkSelector: '.dark-mode',
+    fontFamily: {
+      display: ['system-ui', 'sans-serif'],
+      body: ['system-ui', 'sans-serif']
+    }
+  },
+  variants: {
+    backgroundColor: [
+      'dark',
+      'dark-hover',
+      'dark-group-hover',
+      'dark-even',
+      'dark-odd',
+      'hover',
+      'responsive'
+    ],
+    borderColor: [
+      'dark',
+      'dark-focus',
+      'dark-focus-within',
+      'hover',
+      'responsive'
+    ],
+    textColor: ['dark', 'dark-hover', 'dark-active', 'hover', 'responsive']
+  },
+  plugins: [
+    require('tailwindcss-dark-mode')()
+  ],
+  purgeCSS: {
+    whitelist: ['dark-mode']
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+      // TypeScript
+      'plugins/**/*.ts',
+      'nuxt.config.ts'
+    ]
+  }
+}
