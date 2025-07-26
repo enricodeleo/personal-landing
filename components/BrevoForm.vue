407712a4a49d44e0298fbab1d553f8fec6f8b7d6
Enrico Deleo <hello@enricodeleo.com>
2025-07-26 17:05:39 +0200

    feat: signup

--- components/BrevoForm.vue
+++ components/BrevoForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sib-form w-full" style="text-align: center;">
     <div id="sib-form-container" class="sib-form-container w-full flex justify-items-center">
-      <div id="error-message" class="sib-form-message-panel" style="font-size: 16px; text-align: left; color: #661d1d; background-color: transparent; border-radius: 3px; border-color: #ff4949; max-width: 540px;">
+      <div id="error-message" class="sib-form-message-panel" style="text-align: left; color: #661d1d; background-color: transparent; border-radius: 3px; border-color: #ff4949; max-width: 540px;">
         <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
           <svg viewBox="0 0 512 512" class="sib-icon sib-notification__icon">
             <path d="M256 40C118.621 0 8 119.083 8 256s111.043 216 248 216 248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
@@ -11,7 +11,7 @@
           </span>
         </div>
       </div>
-      <div id="success-message" class="sib-form-message-panel" style="font-size: 16px; text-align: left; color: #085229; background-color: transparent; border-radius: 3px; border-color: #13ce66; max-width: 540px;">
+      <div id="success-message" class="sib-form-message-panel" style="text-align: left; color: #085229; background-color: transparent; border-radius: 3px; border-color: #13ce66; max-width: 540px;">
         <div class="sib-form-message-panel__text sib-form-message-panel__text--center">
           <svg viewBox="0 0 512 512" class="sib-icon sib-notification__icon">
             <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z" />
@@ -22,14 +22,14 @@
         </div>
       </div>
       <div />
-      <div id="sib-container" class="sib-container--large sib-container--vertical" style="text-align: center; background-color: transparent; max-width: 540px; border-radius: 15px; border: none; direction: ltr; margin: 0 auto;">
+      <div id="sib-container" class="sib-container--large sib-container--vertical" style="text-align: center; background-color: #F5F5F5; max-width: 540px; border-radius: 15px; border: none; direction: ltr; margin: 0 auto;">
         <form id="sib-form" method="POST" action="https://b1fa6014.sibforms.com/serve/MUIFAOiLHmcDr6VCxqiw2TGu0PVHOx6kNsMg7GwYNkINCO3q2-QUE2mmEqfUynvm_HociIFYOs7QYvH_Rgzao6x1SIcO71_Cvd36qfhaFS5Zrn7CdRIbr0BekB6pKcxWjt1hKE_h1K5byr06Z4QhGGWtO_NHQ4gIiARex9d3dk1IhBKjoM-fwQ3LQHpEp6FwoL19qHHjF6wXyxeg" data-type="subscription">
           <div style="padding: 8px 0;">
             <div class="sib-input sib-form-block">
               <div class="form__entry entry_block">
                 <div class="form__label-row ">
                   <label class="entry__label" style="font-weight: 700; text-align:left; font-size:16px; color:#3c4858;" for="EMAIL" data-required="*">
-                    Inserisci il tuo indirizzo email per iscriverti
+                    I miei insight + link imperdibili, ogni settimana.
                   </label>
                   <div class="entry__field">
                     <input
@@ -53,7 +53,7 @@
           </div>
           <div style="padding: 8px 0;">
             <div class="sib-form-block" style="text-align: center">
-              <button class="sib-form-block__button sib-form-block__button-with-loader" style="font-size:16px; text-align:center; font-weight:700; color:#000000; background-color:transparent; border-width:0px;" form="sib-form" type="submit">
+              <button class="sib-form-block__button sib-form-block__button-with-loader" style="font-size:16px; text-align:center; font-weight:700; color:#3c4858; background-color:#FFFFFF; border-width:0px;" form="sib-form" type="submit">
                 <svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewBox="0 0 512 512" style="">
                   <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z" />
                 </svg>
@@ -72,24 +72,6 @@
 <script>
 export default {
   name: 'BrevoForm',
-  head () {
-    return {
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://sibforms.com/forms/end-form/build/sib-styles.css'
-        }
-      ],
-      script: [
-        {
-          hid: 'brevo-form',
-          src: 'https://sibforms.com/forms/end-form/build/main.js',
-          defer: true,
-          body: true
-        }
-      ]
-    }
-  },
   mounted () {
     if (typeof window !== 'undefined') {
       window.REQUIRED_CODE_ERROR_MESSAGE = 'Scegli un prefisso paese'
@@ -106,6 +88,24 @@ export default {
         }
       }
       window.AUTOHIDE = Boolean(0)
+    }
+  },
+  head () {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://sibforms.com/forms/end-form/build/sib-styles.css'
+        }
+      ],
+      script: [
+        {
+          hid: 'brevo-form',
+          src: 'https://sibforms.com/forms/end-form/build/main.js',
+          defer: true,
+          body: true
+        }
+      ]
     }
   }
 }
